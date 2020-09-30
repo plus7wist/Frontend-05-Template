c73ac7e6352ff0f6b871c3559c2793516c2d5599
plus7wist <jiazhongguci@foxmail.com>
2020-09-30 19:48:00 +0800

    :sparkles: best move

--- week-01/tictactoe/main.js
+++ week-01/tictactoe/main.js
@@ -4,6 +4,10 @@ const stubX = 1;
 const stubO = 2;
 const range = [0, 1, 2];
 
+const scoreWin = 1;
+const scoreTie = 0;
+const scoreLose = -1;
+
 function playerText(data) {
   if (data === stubX) return playerX;
   if (data === stubO) return playerO;
@@ -14,28 +18,155 @@ function iife(fn) {
   fn();
 }
 
-function createCell(data, world) {
-  const node = document.createElement("div");
-  node.classList.add("cell");
-  node.innerText = playerText(data);
+class Game {
+  constructor() {
+    this.player = 1;
+    this.board = range.map((_) => range.map((_) => new Cell(this)));
+    this.over = false;
+  }
 
-  const onClick = () => {
-    node.innerText = playerText(world.player);
+  isWin() {
+    const board = this.board;
+    const player = this.player;
+    return (
+      range.some((i) => range.every((j) => board[i][j].data === player)) ||
+      range.some((j) => range.every((i) => board[i][j].data === player)) ||
+      range.every((i) => board[i][i].data === player) ||
+      range.every((i) => board[i][2 - i].data == player)
+    );
+  }
 
-    world.player = 3 - world.player;
-  };
-  node.addEventListener("click", onClick);
+  move(cell) {
+    cell.setData(this.player);
+  }
 
-  return node;
+  willWin() {
+    let resultCell;
+
+    const win = range.some((i) =>
+      range.some((j) => {
+        const cell = this.board[i][j];
+        if (cell.data != 0) return false;
+
+        const win = cell.swapData(this.player, (backup, data) => this.isWin());
+        if (!win) return false;
+
+        resultCell = [i, j];
+        return true;
+      })
+    );
+
+    if (!win) return null;
+    return resultCell;
+  }
+
+  // return { score, move }
+  bestMove() {
+    let move = this.willWin();
+    if (move !== null) return { score: scoreWin, move };
+
+    let score = scoreLose;
+    let tie = true;
+
+    range.some((i) =>
+      range.some((j) => {
+        const cell = this.board[i][j];
+        const backup = cell.data;
+
+        if (backup !== 0) return false;
+        tie = false;
+
+        cell.catlikeSetData(this.player);
+        this.togglePlayer();
+        const result = this.bestMove();
+
+        cell.catlikeSetData(backup);
+        this.togglePlayer();
+
+        const myScore = -result.score;
+        if (myScore > score) {
+          score = myScore;
+          move = [i, j];
+        }
+
+        if (myScore === scoreWin) {
+          return true;
+        }
+
+        return false;
+      })
+    );
+    if (tie) return { score: scoreTie, move: null };
+    return { score, move };
+  }
+
+  togglePlayer() {
+    this.player = 3 - this.player;
+  }
+}
+
+class Cell {
+  constructor(game) {
+    this.data = 0;
+    this.clicked = false;
+
+    this.node = document.createElement("div");
+    this.node.classList.add("cell");
+
+    this.setData(0);
+
+    const onClick = () => {
+      if (this.clicked || game.over) return;
+      this.clicked = true;
+
+      game.move(this);
+      if (game.isWin()) {
+        game.over = true;
+        alert(playerText(game.player) + " win!");
+      }
+
+      game.togglePlayer();
+
+      const { score, move } = game.bestMove();
+      console.log(
+        score === scoreWin ? "win" : score === scoreTie ? "tie" : "lose",
+        move
+      );
+    };
+    this.node.addEventListener("click", onClick);
+  }
+
+  setData(data) {
+    this.data = data;
+    this.node.innerText = playerText(data);
+  }
+
+  catlikeSetData(data) {
+    this.data = data;
+  }
+
+  swapData(data, onSwap) {
+    const backup = this.data;
+
+    this.data = data;
+    const result = onSwap(backup, data);
+    this.data = backup;
+
+    return result;
+  }
+
+  isPlayer(player) {
+    return this.data === player;
+  }
 }
 
 iife(() => {
   const world = { player: 1 };
-  const board = range.map((_) => range.map((_) => createCell(0, world)));
+  const game = new Game();
 
   const boardNode = document.getElementById("board");
   range.forEach((i) => {
-    range.forEach((j) => boardNode.appendChild(board[i][j]));
+    range.forEach((j) => boardNode.appendChild(game.board[i][j].node));
     boardNode.appendChild(document.createElement("br"));
   });
 });
