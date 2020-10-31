f5ffcf4dae654bf07858c2ac810fec12f78500d5
plus7wist <jiazhongguci@foxmail.com>
2020-10-31 18:04:02 +0800

    feat: work on wildcard

--- week-04/wildcard.mjs
+++ week-04/wildcard.mjs
@@ -1,14 +1,16 @@
 import { strict as assert } from "assert";
 import { env } from "process";
 
-// KMP table that allow ? match every word.
-function kmpTable(pattern) {
-  const table = new Array(pattern.length).fill(0);
+// KMP table that allow ? match each point.
+function kmpTable(pattern, begin, length) {
+  const table = new Array(length).fill(0);
   let i = 1,
     j = 0;
 
-  while (i < pattern.length) {
-    if (pattern[i] == pattern[j] || pattern[i] == '?' || pattern[j] == '?') {
+  while (i < length) {
+    const pi = pattern[begin + i];
+    const pj = pattern[begin + j];
+    if (pi == pj || pi == "?" || pj == "?") {
       i += 1;
       j += 1;
       table[i] = j;
@@ -27,11 +29,15 @@ function testKmpTable() {
   assert.deepEqual(kmpTable("abababc"), [0, 0, 0, 1, 2, 3, 4]);
 }
 
-function kmp(source, pattern) {
-  const table = kmpTable(pattern);
-  let i = 0, j = 0;
-  while (i < source.length) {
-    if (source[i] === pattern[j] || pattern[j] == '?') {
+function kmp(source, sBegin, sLength, pattern, pBegin, pLength) {
+  const table = kmpTable(pattern, pBegin, pLength);
+  let i = 0,
+    j = 0;
+  while (i < sLength) {
+    const sChar = source[sBegin + i];
+    const pChar = pattern[pBegin + j];
+
+    if (sChar == pChar || pChar == "?") {
       i += 1;
       j += 1;
     } else if (j > 0) {
@@ -39,23 +45,22 @@ function kmp(source, pattern) {
     } else {
       i += 1;
     }
-    if (j == pattern.length) {
-      return i - j;
-    }
+
+    if (j == pLength) return i - j;
   }
   return -1;
 }
 
 function runTest(name, testFn) {
-  console.log('Running test', name);
+  console.log("Running test", name);
   try {
     testFn();
   } catch (error) {
     console.error(error);
-    console.error('Test', name, 'failed');
+    console.error("Test", name, "failed");
     return;
   }
-  console.log('Test', name, 'passed');
+  console.log("Test", name, "passed");
 }
 
 function testKmp() {
@@ -68,7 +73,87 @@ function testKmp() {
   assert.deepEqual(kmp("abcacc", "?cc"), 3);
 }
 
-if (env.MODE == 'debug') {
-  runTest('kmp table', testKmpTable);
-  runTest('kmp', testKmp);
+if (env.MODE == "debug") {
+  runTest("kmp table", testKmpTable);
+  runTest("kmp", testKmp);
 }
+
+function wildcardMatchWithoutStar(source, sBegin, pattern, pBegin, length) {
+  for (let i = 0; i < length; i++) {
+    const sChar = source[i + sBegin];
+    const pChar = pattern[i + pBegin];
+    if (sChar != "?" && pChar != "?" && pChar != sChar) return false;
+  }
+  return true;
+}
+
+function wildcardMatch(source, pattern) {
+  const stars = [];
+  for (let i = 0; i < pattern.length; i++) {
+    if (pattern[i] == "?") {
+      stars.push(i);
+    }
+  }
+
+  if (stars.length == 0) {
+    if (source.length != pattern.length) return false;
+    return wildcardMatchWithoutStar(source, 0, pattern, 0, pattern.length);
+  }
+
+  let sourceBegin = 0;
+  if (
+    stars[0] != 0 &&
+    !wildcardMatchWithoutStar(source, 0, pattern, 0, stars[0])
+  ) {
+    console.log('beginning failed');
+    return false;
+  } else {
+    sourceBegin = stars[0];
+  }
+
+  for (let i = 1; i < stars.length - 1; i++) {
+    const patternBegin = stars[i - 1] + 1;
+    const patternLength = stars[i] - stars[i - 1] - 1;
+    if (patternLength == 0) {
+      continue;
+    }
+    const matchPoint = kmp(
+      source,
+      sourceBegin,
+      source.length - sourceBegin,
+      pattern,
+      patternBegin,
+      patternLength
+    );
+    if (matchPoint == -1) {
+      console.log('middle failed');
+      return false;
+    }
+    sourceBegin = matchPoint + patternLength;
+  }
+
+  const lastStar = stars[stars.length - 1];
+  if (lastStar == source.length - 1) {
+    return true;
+  }
+
+  {
+    const length = pattern.length - lastStar - 1;
+    const sourceBegin = source.length - length;
+    const patternBegin = pattern.length - length;
+    console.log('final match');
+    return wildcardMatchWithoutStar(
+      source,
+      sourceBegin,
+      pattern,
+      patternBegin,
+      length
+    );
+  }
+}
+
+function testWildcardMatch() {
+  assert.deepEqual(wildcardMatch('abc', '*'), true);
+}
+
+testWildcardMatch();
